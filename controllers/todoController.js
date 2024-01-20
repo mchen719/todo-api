2e512272c1a481cec344556516a3f2a0783d958c
Matthew Chen <m.chen71996@mail.com>
2024-01-20 12:51:38 -0500

    Added destroy and update routes

---
+++ controllers/todoController.js
@@ -0,0 +1,61 @@
+const Todo = require('../models/todo.js')
+
+/*
+200 - good response 
+    200 - ok
+    201 - created 
+    204 - no content (everything worked but not sending data back)
+300 - redirection 
+    301 - redirect 
+    302 - redirect
+400 - bad response but it was the user's fault (Server didn't break)
+    400 - bad response 
+    401 - unauthorized 
+    403 - forbidden 
+    404 - not found (try to find something that does not exist)
+500 - server's fault (server didn't work)
+*/
+
+exports.index = async function index(req, res) {
+    try {
+        const todos = await Todo.find({})
+        res.status(200).json(todos)
+    } catch (error) {
+        res.staus(400).json({message: error.message})
+    }
+}
+
+exports.create = async function create(req, res) {
+    try {
+        const todo = await Todo.create(req.body)
+        res.status(200).json(todo)
+    } catch (error) {
+        res.staus(400).json({message: error.message})
+    }
+}
+
+exports.update = async function update(req, res) {
+    try {
+        const updatedTodo = await Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        res.status(200).json(updatedTodo)
+    } catch (error) {   
+        res.staus(400).json({message: error.message})
+    }
+}
+
+exports.destroy = async function destroy(req, res) {
+    try {
+        await Todo.findOneAndDelete({ _id: req.params.id })
+        res.status(200).json({ message: `The todo with the id of ${req.params.id} was deleted from the MongoDB database. No Further action necessary`})
+    } catch (error) {
+        res.staus(400).json({message: error.message})
+    }
+}
+
+exports.show = async function show(req, res) { 
+    try {
+        
+    } catch (error) {
+        
+    }
+}
